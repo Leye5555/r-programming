6d1815275f26e887a9a3665be555c40b3c098074
Olubummo Faith <59509795+Leye5555@users.noreply.github.com>
2025-06-24 17:03:06 +0100

    visuals

--- 24-06-2025.R
+++ 24-06-2025.R
@@ -1,3 +1,9 @@
+install.packages("moments")
+install.packages("ggplot2")
+library(ggplot2)
+library(moments)
+
+
 #Example 1. Mirra is interested in the elapse time (in minutes) she spends on riding a tricycle from home, 
 #at Simandagit, to school, MSU-TCTO, Sanga-Sanga for three weeks (excluding weekends). 
 #She obtain the following data: 19.09, 19.55, 17.89, 17.73, 25.15, 27.27, 25.24, 21.05, 21.65, 20.92, 22.61, 15.71, 22.04, 22.60, and 24.25. 
@@ -7,9 +13,6 @@
 #solution
 
 time <- c(19.09, 19.55, 17.89, 17.73, 25.15, 27.27, 25.24, 21.05, 21.65, 20.92, 22.61, 15.71, 22.04, 22.60, 24.25)
-
-install.packages("moments") 
-library(moments)
 
 skewness(time) #skewness measures the distribution of the data and ensures that the sample is asymmetric. Skewness values of 0, -0.5 to +0.5, are generally considered as insignificant and hence the most idle for symmetry
 
@@ -22,8 +25,7 @@ kurtosis(time)
 
 
 
-install.packages("ggplot2")
-library(ggplot2)
+
 ggplot(data.frame(time), aes(x=time)) + geom_density(color="#ff5fff", alpha=.2)
 
 
