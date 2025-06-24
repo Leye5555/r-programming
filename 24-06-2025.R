b8724b478f949d4dd8ab85ad6e3dd23077ff17a0
Olubummo Faith <59509795+Leye5555@users.noreply.github.com>
2025-06-24 15:20:42 +0100

    skewness and kurtosis

---
+++ 24-06-2025.R
@@ -0,0 +1,28 @@
+#Example 1. Mirra is interested in the elapse time (in minutes) she spends on riding a tricycle from home, 
+#at Simandagit, to school, MSU-TCTO, Sanga-Sanga for three weeks (excluding weekends). 
+#She obtain the following data: 19.09, 19.55, 17.89, 17.73, 25.15, 27.27, 25.24, 21.05, 21.65, 20.92, 22.61, 15.71, 22.04, 22.60, and 24.25. 
+#Compute and interpret the skewness and kurtosis.
+
+
+#solution
+
+time <- c(19.09, 19.55, 17.89, 17.73, 25.15, 27.27, 25.24, 21.05, 21.65, 20.92, 22.61, 15.71, 22.04, 22.60, 24.25)
+
+install.packages("moments") 
+library(moments)
+
+skewness(time) #skewness measures the distribution of the data and ensures that the sample is asymmetric. Skewness values of 0, -0.5 to +0.5, are generally considered as insignificant and hence the most idle for symmetry
+
+kurtosis(time)
+
+
+
+#Interpretation: The data collected has a skewness of negative but insignificant skewness of -0.016, which indicates that the data distribution is symmetric. 
+# It has a platykurtic kurtosis of 2.3 which means that the data is skewed in the negative direction because the computed value is less than 3. That is there are more values on the left side of the mean.  
+
+
+
+
+
+
+
